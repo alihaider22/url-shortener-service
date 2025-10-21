08a1aab8e558eb9a4e892cd8863e9340d8eab25c
Ali Haider <alihaider@foomotion.io>
2025-10-21 19:08:45 +0500

    Refactor: update hashPassword function to accept an optional salt parameter

--- utils/hash.js
+++ utils/hash.js
@@ -1,9 +1,9 @@
 import { randomBytes, createHmac } from "node:crypto";
 
-export const hashPassword = (password) => {
-  const salt = randomBytes(256).toString("hex");
-  const hashedPassword = createHmac("sha256", salt)
+export const hashPassword = (password, salt = undefined) => {
+  const saltToUse = salt ?? randomBytes(256).toString("hex");
+  const hashedPassword = createHmac("sha256", saltToUse)
     .update(password)
     .digest("hex");
-  return { salt, hashedPassword };
+  return { salt: saltToUse, hashedPassword };
 };
