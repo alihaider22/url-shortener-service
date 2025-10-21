abfe79c5bfc2b5daa5601df11a9f3dd35905f4d0
Ali Haider <alihaider@foomotion.io>
2025-10-21 19:09:07 +0500

    Add loginUserSchema validation for user login requests

--- validations/request.validation.js
+++ validations/request.validation.js
@@ -6,3 +6,8 @@ export const createUserSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const loginUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
