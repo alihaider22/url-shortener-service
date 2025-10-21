b6b40ec502c9152286f32e83b71a13ef1bd7b83f
Ali Haider <alihaider@foomotion.io>
2025-10-21 19:09:33 +0500

    Add login route to handle user authentication with JWT token generation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import db from "../db/index.js";
 import { usersTable } from "../models/user.model.js";
-import { createUserSchema } from "../validations/request.validation.js";
+import {
+  createUserSchema,
+  loginUserSchema,
+} from "../validations/request.validation.js";
 import { hashPassword } from "../utils/hash.js";
 import { findUserByEmail } from "../services/user.service.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -38,4 +42,33 @@ router.post("/signup", async (req, res) => {
   });
 });
 
+router.post("/login", async (req, res) => {
+  const validationResult = await loginUserSchema.safeParseAsync(req.body);
+
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.format() });
+  }
+
+  const { email, password } = validationResult.data;
+
+  const existingUser = await findUserByEmail(email);
+
+  if (!existingUser) {
+    return res
+      .status(400)
+      .json({ error: `User wiht email ${email} does not exist` });
+  }
+
+  const { hashedPassword } = hashPassword(password, existingUser.salt);
+
+  if (hashedPassword !== existingUser.password) {
+    return res.status(400).json({ error: "Invalid password" });
+  }
+
+  // generate jwt token
+  const token = jwt.sign({ userId: existingUser.id }, process.env.JWT_SECRET);
+
+  // return token
+  res.status(200).json({ message: "Login successful", token });
+});
 export default router;
